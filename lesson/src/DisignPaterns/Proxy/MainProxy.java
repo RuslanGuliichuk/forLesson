14df81016916fb489a321870909d29591195aa6f
Lerp <kirsebaer123@gmail.com>
2023-10-07 19:01:39 +0300

    Commit

---
+++ lesson/src/DisignPaterns/Proxy/MainProxy.java
@@ -0,0 +1,9 @@
+//package DisignPaterns.Proxy;
+//
+//public class MainProxy {
+//    public static void main(String[] args) {
+//        Project project = new ProxyProject("https://www.github.com/...");
+//
+//        project.run();
+//    }
+//}
